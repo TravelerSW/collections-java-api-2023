2c2a02de770899223abf0b3da204b9576dd4274f
Kevin <95513702+TravelerSW@users.noreply.github.com>
2023-09-11 19:55:45 -0300

    Exemplo sobre ordenação
    Exemplo sobre ordenação de listas em collections em java, consistindo em organizar determinada lista de pessoas por idade e altura.

--- src/main/java/list/Ordenacao/OrdenacaoPessoas.java
+++ src/main/java/list/Ordenacao/OrdenacaoPessoas.java
@@ -1,58 +1,40 @@
 package main.java.list.Ordenacao;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class OrdenacaoPessoas {
-  //atributo
-  private List<Pessoa> pessoaList;
+public class OrdenacaoPessoas{
+  private List<Pessoa>pessoaList;
 
-  public OrdenacaoPessoas() {
+  public OrdenacaoPessoas(){
     this.pessoaList = new ArrayList<>();
   }
 
-  public void adicionarPessoa(String nome, int idade, double altura) {
+  public void adicionarPessoa(String nome, int idade, double altura){
     pessoaList.add(new Pessoa(nome, idade, altura));
   }
 
-  public List<Pessoa> ordenarPorIdade() {
+  public List<Pessoa> ordenarPorIdade(){
     List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
-    if (!pessoaList.isEmpty()) {
-      Collections.sort(pessoasPorIdade);
-      return pessoasPorIdade;
-    } else {
-      throw new RuntimeException("A lista está vazia!");
-    }
+    Collections.sort(pessoasPorIdade);
+    return pessoasPorIdade;
   }
 
-  public List<Pessoa> ordenarPorAltura() {
+  public List<Pessoa> ordenarPorAltura(){
     List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
-    if (!pessoaList.isEmpty()) {
-      Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
-      return pessoasPorAltura;
-    } else {
-      throw new RuntimeException("A lista está vazia!");
-    }
+    Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+    return pessoasPorAltura;
   }
 
   public static void main(String[] args) {
-  // Criando uma instância da classe OrdenacaoPessoas
     OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+    ordenacaoPessoas.adicionarPessoa("Kevin", 21, 1.75);
+    ordenacaoPessoas.adicionarPessoa("Ana", 22, 1.54);
+    ordenacaoPessoas.adicionarPessoa("Eliane", 65, 1.58);
 
-    // Adicionando pessoas à lista
-    ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
-    ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
-    ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
-    ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
-
-    // Exibindo a lista de pessoas adicionadas
-    System.out.println(ordenacaoPessoas.pessoaList);
-
-    // Ordenando e exibindo por idade
-    System.out.println(ordenacaoPessoas.ordenarPorIdade());
-
-    // Ordenando e exibindo por altura
     System.out.println(ordenacaoPessoas.ordenarPorAltura());
+
   }
 }
--- src/main/java/list/Ordenacao/Pessoa.java
+++ src/main/java/list/Ordenacao/Pessoa.java
@@ -2,16 +2,30 @@ package main.java.list.Ordenacao;
 
 import java.util.Comparator;
 
-public class Pessoa implements Comparable<Pessoa> {
-  //atributos
+public class Pessoa implements Comparable<Pessoa>{
   private String nome;
   private int idade;
   private double altura;
 
-  public Pessoa(String nome, int idade, double altura) {
+  public Pessoa(String nome, int idade, double altura){
     this.nome = nome;
-    this.idade = idade;
     this.altura = altura;
+    this.idade = idade;
+  }
+
+  public String getNome(){
+    return nome;
+  }
+  public int getIdade(){
+    return idade;
+  }
+  public double getAltura(){
+    return altura;
+  }
+
+  @Override
+  public String toString(){
+    return "Pessoa: " + nome + ", altura: " + altura + ", idade: " + idade;
   }
 
   @Override
@@ -19,31 +33,10 @@ public class Pessoa implements Comparable<Pessoa> {
     return Integer.compare(idade, p.getIdade());
   }
 
-  public String getNome() {
-    return nome;
-  }
-
-  public int getIdade() {
-    return idade;
-  }
-
-  public double getAltura() {
-    return altura;
-  }
-
-  @Override
-  public String toString() {
-    return "Pessoa{" +
-        "nome='" + nome + '\'' +
-        ", idade=" + idade +
-        ", altura=" + altura +
-        '}';
-  }
 }
-
-class ComparatorPorAltura implements Comparator<Pessoa> {
+class ComparatorPorAltura implements Comparator<Pessoa>{
   @Override
-  public int compare(Pessoa p1, Pessoa p2) {
+  public int compare(Pessoa p1, Pessoa p2){
     return Double.compare(p1.getAltura(), p2.getAltura());
   }
 }
